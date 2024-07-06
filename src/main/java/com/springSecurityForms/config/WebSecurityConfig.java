2246f540c6d35b1f2efa66fa889fc62df42675a2
AnjaneyuluDaddala <anjaneyuludaddala07@gmail.com>
2024-07-06 19:40:09 +0530

    InMemory-success

--- src/main/java/com/springSecurityForms/config/WebSecurityConfig.java
+++ src/main/java/com/springSecurityForms/config/WebSecurityConfig.java
@@ -1,8 +1,5 @@
 package com.springSecurityForms.config;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,75 +7,38 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
-	@Autowired
-	private DataSource dataSource;
 	
-	
-/*
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests(request->request
-				.antMatchers("/home/**").permitAll()
-				.antMatchers("/user/**").hasRole("USER")
+				.antMatchers("/page").permitAll()
+				.antMatchers("/user/**").hasAnyRole("USER","ADMIN")
 				.antMatchers("/admin/**").hasRole("ADMIN")
-				
 				);
-	
-		super.configure(http);
-	}
-	
-	*/
-	
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.jdbcAuthentication().dataSource(dataSource);
-//	}
-
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin(form->form
+				.permitAll());
 		
-		http.authorizeRequests(request->request
-				.antMatchers("/home/**","/h2-console/**").permitAll()
-				.antMatchers("/user/**").hasRole("USER")
-				.antMatchers("/admin/**").hasRole("ADMIN")
-				);
-				
-		http.formLogin(login->login
-				.usernameParameter("username")
-				.loginPage("/login")
-				.permitAll()
-				);
 		http.logout(logout->logout
-				.logoutRequestMatcher( new AntPathRequestMatcher("/logout"))
-				.permitAll()
-				);
+				.permitAll());
 		
-		http.csrf(csrf->csrf
-				.ignoringAntMatchers("/h2-console/**")
-				);
-		http.headers(header->header
-				.frameOptions(frame->frame.sameOrigin())
-				);
-	
 		super.configure(http);
+		
 	}
-
+	
+	  
 	
 	//InMemoryUserDetailsManager
 	
-	/*
+	
 	
 	 @Bean
 	    public UserDetailsService userDetailsService() {
@@ -94,32 +54,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	        return manager;
 	    }
 	 
-	 */
+	 
 
 	    @Bean
 	    public PasswordEncoder passwordEncoder() {
 	        return new BCryptPasswordEncoder();
 	    }
+	  
 
-	    
-	    //JdbcUserDetails
-	    
-	    @Bean
-		public UserDetailsService getDetails() {
-			
-		UserDetails user1=User.withUsername("anjan").password(passwordEncoder().encode("1234"))
-				                             .roles("USER").build();
-		UserDetails admin=User.withUsername("admin").password(passwordEncoder().encode("4567"))
-				                             .roles("ADMIN").build();
-		
-		JdbcUserDetailsManager jdbc=new JdbcUserDetailsManager(dataSource);
-		
-		jdbc.createUser(user1);
-		jdbc.createUser(admin);
-		
-			
-			return jdbc;
-			
-			
-}
+	
 }
--- src/main/java/com/springSecurityForms/controller/FormController.java
+++ src/main/java/com/springSecurityForms/controller/FormController.java
@@ -1,6 +1,8 @@
 package com.springSecurityForms.controller;
 
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,30 +10,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FormController {
 	
-	      
-	    @GetMapping("/login") 
-	    public String login() { 
-	        return "login"; 
-	          
-	    }
 	    
-	    @GetMapping("/home") 
+	    @GetMapping("/page") 
 	    public String home() { 
-	        return "home"; 
+	        return "page"; 
 	          
 	    }
 	    
 	    
 	    
 	    @GetMapping("/user")
-	    public String userLogin(Model model) {
-	    	model.addAttribute("name", "user");
+	    public String userLogin(Model model,Principal prince) {
+	    	model.addAttribute("name", prince.getName());
 	    	return "welcome";
 	    }
 
 	    @GetMapping("/admin")
-	    public String adminLogin(Model model) {
-	    	model.addAttribute("name", "admin");
+	    public String adminLogin(Model model,Principal prince) {
+	    	model.addAttribute("name",prince.getName() );
 	    	return "welcome";
 	    }
 }
