272f65c8e4ca96debf176beaea4bec0a7bd7f89d
AnjaneyuluDaddala <anjaneyuludaddala07@gmail.com>
2024-07-10 21:53:22 +0530

    jdbc-auth

--- src/main/java/com/springSecurityForms/config/WebSecurityConfig.java
+++ src/main/java/com/springSecurityForms/config/WebSecurityConfig.java
@@ -5,8 +5,10 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -122,10 +124,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     	jdbc.setAuthoritiesByUsernameQuery("SELECT username, role as authority FROM user WHERE username = ?");
     
     	
-    	
-	
+   
 		return jdbc;
     }
+
 }
 
 
