fa52d025bd2d392f77064489fe78f24a804cd915
AnjaneyuluDaddala <anjaneyuludaddala07@gmail.com>
2024-07-12 14:55:23 +0530

    cookie-no-security

--- src/main/java/com/springSecurityForms/config/WebSecurityConfig.java
+++ src/main/java/com/springSecurityForms/config/WebSecurityConfig.java
@@ -1,26 +1,28 @@
 package com.springSecurityForms.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
-@Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests(request->request
-				.antMatchers("/page").permitAll()
-				.antMatchers("/user/**","/admin/**").authenticated()
-				);
-		http.formLogin(form->form
-				.permitAll());
-		
-		http.logout(logout->logout
-				.permitAll());
-		
-		super.configure(http);
-		
-	}			
-}
+//@Configuration
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests(request->request
+//				.antMatchers("/page").permitAll()
+//				.antMatchers("/user/**","/admin/**").authenticated()
+//				
+//				
+//				);
+//		http.formLogin(form->form
+//				.loginPage("/login")
+//				.defaultSuccessUrl("/home")
+//				.permitAll());
+//		
+//		http.logout(logout->logout
+//				.permitAll());
+//		
+//		super.configure(http);
+//		
+//	}			
+//}
--- src/main/java/com/springSecurityForms/controller/FormController.java
+++ src/main/java/com/springSecurityForms/controller/FormController.java
@@ -1,12 +1,17 @@
 package com.springSecurityForms.controller;
 
-
 import java.security.Principal;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.springSecurityForms.model.LoginStudent;
 
 @Controller
 public class FormController {
@@ -17,9 +22,7 @@ public class FormController {
 	        return "page"; 
 	          
 	    }
-	    
-	    
-	    
+	    	    
 	    @GetMapping("/user")
 	    public String userLogin(Model model,Principal prince) {
 	    	model.addAttribute("name",prince.getName() );
@@ -31,4 +34,38 @@ public class FormController {
 	    	model.addAttribute("name", prince.getName());
 	    	return "welcome";
 	    }
+	    
+	    @GetMapping("/login")
+	    public String LoginPage(HttpServletRequest request,@ModelAttribute("loginStudent") LoginStudent student) {
+	    	Cookie[] cookies = request.getCookies();
+	    	for (Cookie temp : cookies) {
+	    		if("username".equals(temp.getName())){
+	    			String name=temp.getValue();
+	    			student.setUsername(name);
+	    			}		
+			}
+	    	
+	    	return "login";
+	    }
+	    
+	    @PostMapping("/login")
+        public String checkingLogin(@ModelAttribute("loginStudent") LoginStudent student,
+        		                           HttpServletResponse response) {
+	    	
+	    	Cookie cookie=new Cookie("username",student.getUsername());
+	    	cookie.setMaxAge(60*60*24);
+	    	response.addCookie(cookie);
+	    	
+	    	
+	    	return "redirect:/home";
+	    }
+	    
+	    @GetMapping("/home")
+         public String homePage() {
+	    	return "home";
+	    }
+	   
 }
+
+
+
