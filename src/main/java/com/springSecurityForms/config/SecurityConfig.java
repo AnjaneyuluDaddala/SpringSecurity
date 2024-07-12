e74be7466a68a0acc9bc811ff3e9050d07e36ec8
AnjaneyuluDaddala <anjaneyuludaddala07@gmail.com>
2024-07-12 17:39:10 +0530

    session-fixation

--- src/main/java/com/springSecurityForms/config/SecurityConfig.java
+++ src/main/java/com/springSecurityForms/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -29,8 +28,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
             )
             .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) //a session is created only if required
-                .invalidSessionUrl("/login?invalid-session")
+            		 .sessionFixation().newSession()// creates a new session whenever a user authenticates
+                
             );
     }
 }
